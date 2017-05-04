c2f4da2948b3d0e0cea650f80a2a81653ceed9b0
Ansel Chen <anselcmy@foxmail.com>
2017-05-04 11:53:09 +0800

    Fix set_spider_env and add gitignore

--- set_spider_env.py
+++ set_spider_env.py
@@ -4,14 +4,12 @@ import os
 os.system('mv report_spider report_spider_temp')
 os.system('scrapy startproject report_spider')
 
-os.chdir('./report_spider_temp')
-for f in os.listdir():
+for f in os.listdir('./report_spider_temp'):
 	if os.path.isdir(f):
-		os.system('cp -r ./{} ../report_spider'.format(f))
+		os.system('cp -r ./report_spider_temp/{} ./report_spider'.format(f))
 	else:
-		os.system('cp ./{} ../report_spider'.format(f))
+		os.system('cp ./report_spider_temp/{} ./report_spider'.format(f))
 
-os.chdir('../')
 os.system('rm -r -f report_spider_temp')
 
 os.chdir('./report_spider')
